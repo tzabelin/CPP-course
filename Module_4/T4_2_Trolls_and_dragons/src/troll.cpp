99ca8ee6f86dbe2543780c0501d5655f07eb935b
unknown <zabelin.timofei@gmail.com>
2024-09-19 12:06:48 +0300

    4.2

--- Module_4/T4_2_Trolls_and_dragons/src/troll.cpp
+++ Module_4/T4_2_Trolls_and_dragons/src/troll.cpp
@@ -1,3 +1,9 @@
 #include "troll.hpp"
 
+class Troll : public Creature {
+public:
+    // Construct Troll, use following arguments for Creature initialization
+    Troll(const std::string& name) : Creature(name, "Troll", 10) { }
+    virtual std::string WarCry() const { return "Ugazaga!"; }
+};
 
