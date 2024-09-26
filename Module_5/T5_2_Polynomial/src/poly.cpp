147a1fc48795b186dad54e287e299929a5088055
unknown <zabelin.timofei@gmail.com>
2024-09-26 17:12:09 +0300

    5.2

--- Module_5/T5_2_Polynomial/src/poly.cpp
+++ Module_5/T5_2_Polynomial/src/poly.cpp
@@ -84,3 +84,92 @@ bool operator!=(const Poly& a, const Poly& b)
     else
      return false;
 }
+
+std::istream& operator>>(std::istream& is, Poly& p)
+{
+    std::string input;
+    is >> input;
+    std::stringstream ss(input);
+    p = Poly();
+    int sign = 1;
+    bool first_term = true;
+    while (ss)
+    {
+        ss >> std::ws; 
+        char c = ss.peek();
+        if (c == '+')
+        {
+            ss.get();
+            sign = 1;
+        }
+        else if (c == '-')
+        {
+            ss.get();
+            sign = -1;
+        }
+        else if (first_term)
+        {
+            sign = 1;
+        }
+        else if (ss.eof())
+        {
+            break;
+        }
+
+        first_term = false;
+
+        int mul;
+        if (!(ss >> mul))
+        {
+            is.setstate(std::ios::failbit);
+            return is;
+        }
+
+        char x;
+        if (!(ss >> x) || x != 'x')
+        {
+            is.setstate(std::ios::failbit);
+            return is;
+        }
+
+        int exp;
+        if (!(ss >> exp))
+        {
+            is.setstate(std::ios::failbit);
+            return is;
+        }
+
+        mul *= sign;
+        p[exp] = mul;
+    }
+    return is;
+}
+
+std::ostream& operator<<(std::ostream& os, const Poly& p)
+{
+    bool first_term = true;
+    for (auto it = p.begin(); it != p.end(); ++it)
+    {
+        int exp = it->first;
+        int mul = it->second;
+        if (mul == 0)
+            continue;
+
+        if (first_term)
+        {
+            if (mul < 0)
+                os << "-";
+            os << std::abs(mul) << "x" << exp;
+            first_term = false;
+        }
+        else
+        {
+            if (mul > 0)
+                os << "+";
+            else
+                os << "-";
+            os << std::abs(mul) << "x" << exp;
+        }
+    }
+    return os;
+}
