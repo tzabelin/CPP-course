3835dc168811f7afad47e8f2313cdae6867820b2
unknown <zabelin.timofei@gmail.com>
2024-10-03 23:29:30 +0300

    5.1

--- Module_5/T5_1_Vehicle_register/src/register.cpp
+++ Module_5/T5_1_Vehicle_register/src/register.cpp
@@ -1,5 +1,89 @@
+
 #include "register.hpp"
-#include "aircraft.hpp"
-#include "boat.hpp"
 #pragma once
+
 //TODO: implement Register's members
+
+void Register::Add(Vehicle* v)
+{
+    this->vehicles_.push_back(v);
+}
+void Register::Save(const std::string& filename) const
+{
+    std::ofstream stream(filename);
+    for(auto vehicle : this->vehicles_)
+    {
+        vehicle->Write(stream);
+    }
+}
+
+Register::~Register() 
+{
+    for (auto vehicle : vehicles_) 
+    {
+        delete vehicle;
+    }
+}
+
+bool Register::ReadLine(std::istream& stream)
+{
+    std::string line;
+    if (!std::getline(stream, line))
+        return false;
+    std::istringstream str(line);
+    char type;
+    if (!(str >> type))
+        return false;
+    if (str.get() != ';')
+        return false;
+    Vehicle* vehicle = nullptr;
+    if (type == 'A')
+    {
+        vehicle = Aircraft::Read(str);
+    }
+    else if (type == 'B')
+    {
+        vehicle = Boat::Read(str);
+    }
+    else
+    {
+        return false;
+    }
+    if (vehicle != nullptr)
+    {
+        Add(vehicle);
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+int Register::Load(const std::string& filename)
+{
+    std::ifstream stream(filename);
+    int vehicles_added = 0;
+    while (stream)
+    {
+        bool result = ReadLine(stream);
+        if (stream.eof())
+            break;
+        if (result)
+            vehicles_added++;
+    }
+    return vehicles_added;
+}
+
+void Register::Print()
+{
+    for(auto vehicle : this->vehicles_)
+    {
+        vehicle->Print();
+    }
+}
+
+size_t Register::Size() const
+{
+    return this->vehicles_.size();
+}
--- Module_5/T5_1_Vehicle_register/src/vehicle.cpp
+++ Module_5/T5_1_Vehicle_register/src/vehicle.cpp
@@ -1,3 +1,16 @@
+
 #include "vehicle.hpp"
-//TODO: implement Vehicle's members
 #pragma once
+
+//TODO: implement Vehicle's members
+
+
+std::string Vehicle::GetRegisterNumber() const
+{
+    return this->register_number_;
+}
+std::string Vehicle::GetOwner() const
+{
+    return this->owner_;
+}
+Vehicle::Vehicle(std::string register_number, std::string owner) : register_number_(register_number), owner_(owner) {}
