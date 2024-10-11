e0456752617bbe980e16de2ac25809f81b85be48
unknown <zabelin.timofei@gmail.com>
2024-10-11 15:14:43 +0300

    6.4

---
+++ Module_6/T6_2_StrPrinter/src/diagonal_printer.cpp
@@ -0,0 +1,30 @@
+#include "diagonal_printer.hpp"
+
+DiagonalPrinter::DiagonalPrinter(const std::string& first_line, const std::string& last_line, std::ostream& os) : StringPrinter(os), first_line_(first_line), last_line_(last_line) {}
+
+StringPrinter* DiagonalPrinter::Clone() const 
+{
+    return new DiagonalPrinter(first_line_, last_line_, os_);
+}
+
+StringPrinter& DiagonalPrinter::operator()(const std::string& str) 
+{
+    if (!first_line_.empty()) 
+    {
+        os_ << first_line_ << '\n';
+    }
+
+    int i = 0;
+    int n = str.length();
+    for (char c : str) 
+    {
+        os_ << std::string(i, ' ') << c <<  std::string(n - 1 - i, ' ') << '\n';
+        i += 1;
+    }
+    if (!last_line_.empty()) 
+    {
+        os_ << last_line_ << '\n';
+    }
+
+    return *this;
+}
--- Module_6/T6_2_StrPrinter/src/standard_printer.cpp
+++ Module_6/T6_2_StrPrinter/src/standard_printer.cpp
@@ -1,1 +1,11 @@
-
+#include "standard_printer.hpp"
+StandardPrinter::StandardPrinter(std::ostream& os) : StringPrinter(os) {}
+StringPrinter* StandardPrinter::Clone() const 
+{
+    return new StandardPrinter(os_);
+}
+StringPrinter& StandardPrinter::operator()(const std::string& str) 
+{
+    os_ << str << '\n';
+    return *this;
+}
